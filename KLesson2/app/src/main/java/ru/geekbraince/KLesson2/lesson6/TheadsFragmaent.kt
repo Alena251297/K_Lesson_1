5bf7efbc6becbe8c05311ee44df410bd2373a7d6
alena <alenkashved@gmail.com>
2023-01-27 16:25:59 +0300

    Lesson6 27/01/2023 создали вечный поток MyThreads

--- KLesson2/app/src/main/java/ru/geekbraince/KLesson2/lesson6/TheadsFragmaent.kt
+++ KLesson2/app/src/main/java/ru/geekbraince/KLesson2/lesson6/TheadsFragmaent.kt
@@ -36,36 +36,64 @@ class TheadsFragmaent:Fragment() {
             val timer = binding.editText.text.toString().toInt()
             startCalculations(timer)
         }
+        val myHandler1991 = Handler(Looper.myLooper()!!) //ссылка на текущий поток(главный)
+        binding.calculationTheadsBtn.setOnClickListener {
+                      Thread {
+//                val myHandler1992 = Handler(Looper.myLooper()!!)//ссылка на главный
+                val myHandler1993 =
+                    Handler(Looper.getMainLooper()!!)//ссылка на текущий поток(Thead вспомогательный)
+                val timer = binding.editText.text.toString().toInt()
+                startCalculations(timer)
+                myHandler1991.post(Runnable {//работа с view
+                    binding.mainContainer.addView(TextView(it.context).apply {
+                        text = "Thead1991"
+                        textSize = 30f
+                    })
+                })
+                myHandler1993.post(Runnable {//работа с view
+                    binding.mainContainer.addView(TextView(it.context).apply {
+                        text = "Thead1993"
+                        textSize = 30f
+                    })
+                })
 
-            val myHandler1991 = Handler(Looper.myLooper()!!) //ссылка на текущий поток(главный)
-            Thread{
-                binding.calculationTheadsBtn.setOnClickListener {
-                    val myHandler1992 = Handler(Looper.myLooper()!!)//ссылка на главный
-                    val myHandler1993 = Handler(Looper.getMainLooper()!!)//ссылка на текущий поток(Thead вспомогательный)
-                    val timer = binding.editText.text.toString().toInt()
-                    startCalculations(timer)
-                    myHandler1991.post(Runnable {//работа с view
-                        binding.mainContainer.addView(TextView(it.context).apply {
-                            text = "Thead1991"
-                            textSize = 30f
-                        })
-                    })
-                    myHandler1993.post(Runnable {//работа с view
-                        binding.mainContainer.addView(TextView(it.context).apply {
-                            text = "Thead1993"
-                            textSize = 30f
-                        })
-                    })
-                }
             }.start()
+        }
 
+        val handlerTheard = MyThread() //вечный поток
+        handlerTheard.start()
+
+        binding.calcTheadsHandlerBtn.setOnClickListener {
+            val handler = handlerTheard.mHandler
+            handler?.post{//задачи
+                val timer = binding.editText.text.toString().toInt()
+                startCalculations(timer)
+                myHandler1991.post(Runnable {//работа с view
+                    binding.mainContainer.addView(TextView(it.context).apply {
+                        text = "Handler Theads"
+                        textSize = 30f
+                    })
+                })
+            }
+        }
+        handlerTheard.mHandler?.looper?.quitSafely()//ожидает завершения всех задач
+        handlerTheard.mHandler?.looper?.quit() //выходим незамедлительно
     }
     private fun startCalculations(timer: Int){
         val currentTime = Date().time // Date().time - возвращает текущее время в милисекундах на устройстве
         while ((currentTime+timer*1000)>Date().time){}
     }
+}
+
+//создаем свой поток
+class MyThread:Thread(){
+    var mHandler:Handler?=null
+    override fun run() {
+        //бесконечный луп, зацикленный поток
+        Looper.prepare()
+        mHandler = Handler(Looper.myLooper()!!)
+        Looper.loop()
     }
-
-
+}
 
 
