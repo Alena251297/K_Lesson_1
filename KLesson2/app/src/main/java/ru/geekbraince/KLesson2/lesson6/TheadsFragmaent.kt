fddb8058f9b71e4b3f5033367a141b9ff49bc909
alena <alenkashved@gmail.com>
2023-01-27 15:52:38 +0300

    Lesson6 27/01/2023 Начали Урок 6 многопоточность и сервисы, создали package lesson6, создали фрагмент, поработали с потоками, Handler, Looper, MQ. Почитать еще дополнительно про потоки.

---
+++ KLesson2/app/src/main/java/ru/geekbraince/KLesson2/lesson6/TheadsFragmaent.kt
@@ -0,0 +1,71 @@
+package ru.geekbraince.KLesson2.lesson6
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import ru.geekbraince.KLesson2.databinding.FragmentTheaderBinding
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class TheadsFragmaent:Fragment() {
+    companion object{
+        fun newInstance()=TheadsFragmaent()
+    }
+
+    private var _binding:FragmentTheaderBinding?=null
+    private val binding:FragmentTheaderBinding
+    get()=_binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =FragmentTheaderBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState) //Danger!!!
+        binding.button.setOnClickListener {
+            val timer = binding.editText.text.toString().toInt()
+            startCalculations(timer)
+        }
+
+            val myHandler1991 = Handler(Looper.myLooper()!!) //ссылка на текущий поток(главный)
+            Thread{
+                binding.calculationTheadsBtn.setOnClickListener {
+                    val myHandler1992 = Handler(Looper.myLooper()!!)//ссылка на главный
+                    val myHandler1993 = Handler(Looper.getMainLooper()!!)//ссылка на текущий поток(Thead вспомогательный)
+                    val timer = binding.editText.text.toString().toInt()
+                    startCalculations(timer)
+                    myHandler1991.post(Runnable {//работа с view
+                        binding.mainContainer.addView(TextView(it.context).apply {
+                            text = "Thead1991"
+                            textSize = 30f
+                        })
+                    })
+                    myHandler1993.post(Runnable {//работа с view
+                        binding.mainContainer.addView(TextView(it.context).apply {
+                            text = "Thead1993"
+                            textSize = 30f
+                        })
+                    })
+                }
+            }.start()
+
+    }
+    private fun startCalculations(timer: Int){
+        val currentTime = Date().time // Date().time - возвращает текущее время в милисекундах на устройстве
+        while ((currentTime+timer*1000)>Date().time){}
+    }
+    }
+
+
+
+
--- KLesson2/app/src/main/java/ru/geekbraince/KLesson2/view/MainActivity.kt
+++ KLesson2/app/src/main/java/ru/geekbraince/KLesson2/view/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import androidx.annotation.RequiresApi
 import ru.geekbraince.KLesson2.R
 import ru.geekbraince.KLesson2.databinding.ActivityMainBinding
 import ru.geekbraince.KLesson2.databinding.ActivityMainWebViewBinding
+import ru.geekbraince.KLesson2.lesson6.TheadsFragmaent
 import ru.geekbraince.KLesson2.view.main.MainFragment
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -27,9 +30,26 @@ class MainActivity : AppCompatActivity() {
         if(savedInstanceState == null)
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_conteiner, MainFragment.newInstance()).commit()
-
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            //меню сработало
+            R.id.action_open_menu_fragment_theads->{ //если id совпадает, то помещаем нашу менюшку
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_conteiner, TheadsFragmaent.newInstance()).commit()
+                true
+            }
+            //меню не сработало
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
 //if(savedInstanceState==null)
 //        supportFragmentManager.beginTransaction()
